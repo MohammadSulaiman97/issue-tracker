119548111fd8a6eb476afa5714cffcdf2986e5aa
Mohammad Sulaiman <mohammad.sulaiman@vianoes.com>
2024-08-11 10:59:43 +0400

    classnames - selected item in navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,20 +1,30 @@
+'use client';
+
+import classnames from 'classnames';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { FaBug } from "react-icons/fa";
 
 const NavBar = () => {
+    const currentPath = usePathname();
     const links = [
         { label: 'Dashboard', href: '/' },
         { label: 'Issues', href: '/issues' },
     ];
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center text-zinc-500'>
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center text-zinc-900'>
         <Link href="/"><FaBug /></Link>
         <ul className='flex space-x-6'>
             {links.map(link => 
                 <Link 
                     key={link.href} 
-                    className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+                    className={classnames({
+                        'text-zinc-900' : link.href === currentPath,
+                        'text-zinc-500' : link.href !== currentPath,
+                        'hover:text-zinc-800 transition-colors' : true
+                        })
+                    } 
                     href={link.href}>{link.label}
                 </Link>
             )}
