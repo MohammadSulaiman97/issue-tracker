131bdc546e7a5a3c6bf0c59e4da12c7dab328f99
Mohammad Sulaiman <mohammad.sulaiman@vianoes.com>
2024-08-21 15:51:01 +0400

    Filter issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -3,9 +3,25 @@ import prisma from '@/prisma/client'
 import { IssueStatusBadge, Link } from '@/app/components'
 // import delay from "delay";
 import IssueActions from './IssueActions'
+import { Status } from '@prisma/client'
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany(); 
+
+interface Props { 
+  searchParams: { status: Status }
+}
+
+const IssuesPage = async ({ searchParams } : Props) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status) 
+    ? searchParams.status
+    : undefined;
+
+  // const issues = await prisma.issue.findMany(); 
+  const issues = await prisma.issue.findMany({
+    where: {
+      status
+    }
+  });
   // await delay(2000);
   
   return (
